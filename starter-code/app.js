708ca732d77e2cd30ec4f884b3cc66131e72bde8
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-08 13:18:17 +0100

    Iteration 1 complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,19 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+// our first Route:
+let data = {
+  chuckValue: ""
+};
+app.get('/random', (request, response, next) => {
+  client.getRandomJoke().then(response => {
+    data.chuckValue = response.value;
+  });
+   response.render('index.ejs',data);
+  next();
+});
+
+// Server Started
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
